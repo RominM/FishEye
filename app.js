d0fc5e345072109fcd7f5f82089239fb185400cb
RominM <rominmanogil.off@gmail.com>
2021-06-17 12:03:59 +0200

    struct dynamic DOM

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
-fetch("./data.json")
+fetch("./data.json")// le 'fetch' est une promesse mais ne donnera la reponse que lorsqu'il aura recupérer les données.
+// il n'empeche, cependant, pas la lecture du fichier
     .then(response => { 
         return response.json()
     })
     .then(reponse2 => {
 
-
-        init(rep);
+        init(reponse2);
     })
 
-
+// tous ce qui suivera sera effectuer même si 'fetch' n'a pas encore reçu de reponse.
 const init = (data) => {
 
+    //boucle pour chaque photographes
     data.photographers.forEach(photographer => {
-        
-        //creation element creatElement
-        //parametre de l'element classList
-        //append = ajoute
 
-        const card = document.createElement('div');
-        const contentLink = document.createElement('a');
-
-        card.classList('cards');
-        contentLink.classList('contentLink');
-        contentLink.href="toto.fr";
-
-        card.append(contentLink);
-        
+        // element du DOM necessaire
         const mainContent = document.querySelector('.mainContent');
+
+        //creation des elements
+        const card = document.createElement('div');
+        const link = document.createElement('a');
+        const image = document.createElement('img');
+        const nameTitle = document.createElement('h2');
+        const para = document.createElement('p');
+        const place = document.createElement('span');
+        const gimmick = document.createElement('span');
+        const price = document.createElement('span');
+
+        //parametre de l'element (class, id, alt, src, ...)
+        card.classList('cards');
+        link.classList('contentLink');
+        link.href="exemple.fr";
+        image.src="exemple.pgn";
+        image.alt="Portrait Nora";
+        nameTitle.innerHTML = "photographer.name";
+        para.classList('descript');
+        place.classList('local');
+        gimmick.classList('depiction');
+        price.classList('price');
+        
+        //append = ajoute/indenter
         mainContent.append(card);
+
+        card.append(link);
+        link.append(image);
+        link.append(nameTitle);
+        link.append(para);
+        para.append(place);
+        para.append(gimmick);
+        para.append(price);
     });
 }
