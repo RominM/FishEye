1e046780bc4de8bd74cf396534e38f3f3ce27e64
RominM <rominmanogil.off@gmail.com>
2021-06-22 17:10:23 +0200

    first approach for media

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 fetch("./data.json") // le 'fetch' est une promesse mais ne donnera la reponse que lorsqu'il aura recupérer les données.
-   // il n'empeche, cependant, pas la lecture du fichier
-   .then(response => {
+// il n'empeche, cependant, pas la lecture du fichier
+//ici, le fetch demande des information concernant le fichier 'data.json"
+   .then(response => {//ici sera decrit sa reponse
       return response.json()
-   })
+   })//en attendant cette reponse, on lui demande d'initialiser la response2
    .then(response2 => {
       init(response2);
    })
 
-// tous ce qui suivera sera effectuer même si 'fetch' n'a pas encore reçu de reponse.
-const init = (data) => {
+   
+   // tous ce qui suivera sera effectuer même si 'fetch' n'a pas encore reçu de reponse.
+   const init = (data) => {
+   console.log(data);
    //boucle pour chaque photographes
    data.photographers.forEach(photographer => {
-      
       //DOM ELEMENTS
       const mainContent = document.querySelector('.mainContent');
 
+      //___________//create
+      //HEADER_____
+      const header = document.createElement('header');
+      const linkHome = document.createElement('a');
+      const logo = document.createElement('img');
+      const nav = document.createElement('nav');
       //___________//create
       //CARDS______
       const card = document.createElement('div');
@@ -26,11 +34,19 @@ const init = (data) => {
       const place = document.createElement('span');
       const gimmick = document.createElement('span');
       const price = document.createElement('span');
-      // const mainFlux = document.createElement('section');
+      const mainFlux = document.createElement('section');
       //__________//create
       //TAGS______
       const ul = document.createElement('ul');
 //**************************************************************************************/
+      //___________//settings
+      //HEADER_____
+      header.classList.add('banner');
+      linkHome.classList.add('logo');
+      logo.scr = "FishEye_Photos/logo.png";
+      logo.alt = "FishEye Home Page";
+      nav.classList.add('topNav');
+      nav.ariaLabel = "photographer categories"; 
       //___________//settings
       //CARDS______
       card.classList.add('cards');
@@ -48,13 +64,20 @@ const init = (data) => {
       gimmick.innerHTML = photographer.tagline;
       price.classList.add('price');
       price.innerHTML = photographer.price + '€/jour';
-      // mainFlux.classList.add('mainFlux');
+      mainFlux.classList.add('mainFlux');
       //__________//settings
       //TAGS______
       ul.classList.add('tagDesign');
       /*********inDOM*********/
       mainContent.append(card);
+
 //**************************************************************************************/
+      //___________//indent
+      //HEADER_____
+      header.append(linkHome);
+      linkHome.append(logo);
+      header.append(nav);
+      nav.append(ul);
       //___________//indent
       //CARDS______
       card.append(image);
@@ -65,9 +88,9 @@ const init = (data) => {
       para.append(place);
       para.append(gimmick);
       para.append(price);
-      // card.append(mainFlux);
+      card.append(mainFlux);
       //__________//indent
-      //TAGS______.
+      //TAGS______
       for(let i=0; i<photographer.tags.length; i++){
          const li = document.createElement('li')
          const tag = document.createElement('span');
@@ -79,7 +102,7 @@ const init = (data) => {
          ul.append(li);
       }
       blockFlex.append(ul);
-
+      
 //**************************************************************************************/
       const photographerPageActive = () => {
          // mainContent.style.zIndex = -1;
@@ -87,14 +110,40 @@ const init = (data) => {
          card.className = 'card2';
          blockFlex.className = 'blockFlex2';
          contact.style.display = 'block';
-         // mainFlux.style.display = 'block';
+         mainFlux.style.display = 'block';
       }
       card.addEventListener('click', () => {
          photographerPageActive();
          // card.style.border = 'solid red 2px'
       })
    })
+
+   data.media.forEach(photographerId => {
+      console.log(data.media[0].photographerId); //Id du 1er photographe
+      
+      //_________//indent
+      //MEDIA____
+      for(let i=0; i<media.photographerId; i++){
+         
+         console.log(media.photographerId);
+         const album = document.createElement('div');
+         const photo = document.createElement('img');
+
+         album.classList.add('album');
+         photo.classList.add('pic');
+         photo.src = data.media[i].image;
+
+         mainFlux.append(album);
+         album.append(photo)
+         }
+      
+      
+   });
+
+   
 }
+
+
 
 
 
