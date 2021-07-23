10b29e859f9f3bde677c5b1c48ebb6341f63e511
RominM <rominmanogil.off@gmail.com>
2021-07-23 19:08:09 +0200

    Lightbox and interactive heart liked

--- photographerPage.js
+++ photographerPage.js
@@ -124,7 +124,8 @@ head.classList.add('banner');
 section.classList.add('presentation');
 linkHome.classList.add('logo');
 linkHome.href = "./index.html";
-logo.alt = "FishEye Home Page";
+logo.alt = "FishEye page d'accueil";
+logo.title = "FishEye page d'accueil";
 logo.src = "FishEye_Photos/logo.png";
 //___________//indent
 //HEADER_______
@@ -164,12 +165,15 @@ gimmick.classList.add('depiction');
 gimmick.innerHTML = photographer.tagline;
 divTitle.classList.add('divTitle');
 contact.classList.add('contact');
-contact2.classList.add('contact2');
 contact.innerHTML = "Contactez-moi";
+contact.title = "Contactez-moi";
+contact2.classList.add('contact2');
 contact2.innerHTML = "Contactez-moi";
+contact2.title = "Contactez-moi";
 photographerFace.classList.add('photographerFace');
 image.src = "FishEye_Photos/Photographers ID Photos/" + photographer.portrait;
 image.alt = 'photo portrait de ' + photographer.name;
+image.title = "photo portrait de " + photographer.name;
 blockFlex.classList.add('blockFlex');
 //__________//settings
 //TAGS______
@@ -288,7 +292,7 @@ blackHeart.classList.add('infoBox__blackHeart');
 blackHeartImg.src = "./FishEye_Photos/heart-black.svg";
 blackHeartImg.alt = "coeur noir";
 infoPrice.classList.add('infoBox__infoPrice');
-infoPrice.innerHTML = photographer.price + "€ / jours"
+infoPrice.innerHTML = photographer.price + "€ / jour"
 //____________//indent
 album.append(infoBox);
 infoBox.append(likeBox);
@@ -314,27 +318,41 @@ medias.forEach(media => {
    const heartImg = document.createElement('img');
 
    if (media.video) {
+      //_____________//create
+      const linkVid = document.createElement('a');
       const vid = document.createElement('video');
       const source = document.createElement('source');
+      //_____________//settings
+      linkVid.href = "./FishEye_Photos/" + photographer.name + "/" + media.video;
       vid.controls = "true";
       vid.type = "video/mp4"
+      vid.title = media.title + " date " + media.date + " prix " + media.price + "€";
       source.src = "./FishEye_Photos/" + photographer.name + "/" + media.video;
-      source.alt = media.title + ", date: " + media.date + ", prix: " + media.price + "€";
+      source.alt = media.title + " date " + media.date + " prix " + media.price + "€";
       vid.classList.add('fig-vid');
-      divPhoto.append(vid);
+      //_____________//indent
+      divPhoto.append(linkVid);
+      linkVid.append(vid);
       vid.append(source);
    } else if (media.image) {
+      //_____________//create
+      const linkPic = document.createElement('a');
       const pic = document.createElement('img');
+      //_____________//settings
+      linkPic.href = "./FishEye_Photos/" + photographer.name + "/" + media.image;
       pic.src = "./FishEye_Photos/" + photographer.name + "/" + media.image;
-      pic.alt = media.title + ", date: " + media.date + ", prix: " + media.price + "€";
+      pic.alt = media.title + " date " + media.date + " prix " + media.price + "€";
+      pic.title = media.title + " date " + media.date + " prix " + media.price + "€";
       pic.classList.add('fig-img')
-      divPhoto.append(pic);
+      //_____________//indent
+      divPhoto.append(linkPic);
+      linkPic.append(pic);
    }
    //_____________//settings
    //ALBUM________
    albumPhoto.classList.add('albumPhoto');
    divPhoto.classList.add('divPhoto');
-   divPhoto.href = "./FishEye_Photos/" + photographer.name + "/" + media.image;
+   divPhoto.src = "./FishEye_Photos/" + photographer.name + "/" + media.image;
    picSubtitle.classList.add('subtitle');
    nameImg.classList.add('nameImg');
    nameImg.innerHTML = media.title;
@@ -343,8 +361,7 @@ medias.forEach(media => {
    like.classList.add('like');
    like.innerHTML = media.likes;
    heart.classList.add('heart');
-   heartImg.src = "./FishEye_Photos/heart-red.svg";
-   heartImg.alt = media.likes + " like";
+   heartImg.classList.add('heartImg');
    //___________//indent
    //ALBUM______
    main.append(album);
@@ -356,11 +373,10 @@ medias.forEach(media => {
    picSubtitle.append(blockLike);
    blockLike.append(like);
    blockLike.append(heart);
-   heart.append(heartImg);
 
    //Addition des likes
    totalLike += media.likes;
-
+   infoBox.title = 'Like total et tarif de ' + photographer.price + '€ par jour';
 })
 
 //TRIER PAR
@@ -369,31 +385,33 @@ const getMedias = document.querySelectorAll('.albumPhoto');
 
 for (let i = 0; i < buttons.length; i++) {
    let self = buttons[i];
-   console.log(buttons[i].textContent);
 
    self.addEventListener('click', function () {
 
-         // trie par Popularité
+      // trie par Popularité
       if (buttons[i].textContent === 'Popularité') {
          const sortPop = [];
          for (let i = 0; i < medias.length; i++) {
-            medias.sort((a, b) => a.likes - b.likes);
             sortPop.push(medias[i].likes);
+            console.log(medias[i].likes);
+            medias.sort((a, b) => a.likes - b.likes);
          }
          // trie par Date
       } else if (buttons[i].textContent === 'Date') {
          const sortDate = [];
          for (let i = 0; i < medias.length; i++) {
             medias.sort((a, b) => {
+               sortDate.push(medias[i].date);
+               console.log(medias[i].date);
                return new Date(b.date) - new Date(a.date);
             });
-            sortDate.push(medias[i].date);
          }
          // trie par Titre
       } else if (buttons[i].textContent === 'Titre') {
          const sortTitle = [];
          for (let i = 0; i < medias.length; i++) {
             sortTitle.push(medias[i].title);
+            console.log(medias[i].title);
             medias.sort();
          }
       }
@@ -407,12 +425,11 @@ for (let i = 0; i < buttons.length; i++) {
       })
 
       for (let i = 0; i < getMedias.length; i++) {
-         sortToDisplay.push(getMedias[i]); 
-         console.log(getMedias[i]);
+         sortToDisplay.push(getMedias[i]);
       }
 
       for (let i = 0; i < sortToDisplay.length; i++) {
-         console.log(sortToDisplay[i]);
+         // console.log(sortToDisplay[i]);
          sortToDisplay[i].style.display = "block";
       }
    });
@@ -430,11 +447,11 @@ const incrementPic = () => {
          const likeNmb = parseInt(like.innerHTML);
 
          if (heart.classList.contains('clicked') == false) {
+            const heartImg = document.querySelector('.heartImg');
             heart.classList.add('clicked');
             totalLike += 1;
             like.innerHTML = likeNmb + 1;
             displayTotalLike();
-            console.log(totalLike);
 
          } else {
             heart.classList.remove('clicked');
@@ -451,68 +468,141 @@ const displayTotalLike = () => {
    likeCounter.innerHTML = totalLike;
 }
 
-
 displayTotalLike();
 incrementPic();
 
-
-
-
-
-
-
-
-
-
-
-
 //******************* LIGHTBOX *********************/
 
+/**
+ * @property {HTMLElement} element
+ * @property {string[]} images Chemins des images de la Lightbox
+ * @property {string} url Image actuellement affichée
+ */
 class lightbox {
-
+   // initialisation de la lightbox
+   // Objectif :
+   // greffer un comportement sur tous les liens qui menent à des images. Ouverture de la Lightbox lorsqu'on click sur un de ces liens
    static init() {
-      const links = document.querySelectorAll('.fig-vid, .fig-img')
-      links.forEach(link => link.addEventListener('click', e => {
-         e.preventDefault()
-         new lightbox(e.currentTarget.getAttribute('href'))
-      }))
+
+      const links = Array.from(document.querySelectorAll('a[href$=".jpg"], a[href$=".mp4"]'));
+      const gallery = links.map(link => link.getAttribute('href'))
+
+         links.forEach(link => link.addEventListener('click', e => {
+            e.preventDefault() // strop le comportement par defaut
+            new lightbox(e.currentTarget.getAttribute('href'), gallery) // permet de selectionner le lien sur lequel j'appuie et je recupere l'attribut "href"(urel du lien)
+         }))
    }
-   constructor(url) {
-      const element = this.buildDOM(url);
-      body.append(element);
+
+   //*********CONSTRUCTOR**********//
+   /**
+    * 
+    * @param {string} url url de l'image
+    * @param {string[]} images Chemins des images de la Lightbox
+    */
+   constructor(url, images) {
+      this.element = this.buildDOM(url);
+      this.images = images;
+      this.loadImage(url);
+      this.onKeyUp = this.onKeyUp.bind(this);
+      document.body.appendChild(this.element);
+      document.addEventListener('keyup', this.onKeyUp);
    }
+
+   //*********lOADIMAGE************//
+
+   /**
+    * 
+    * @param {string} url url de l'image
+    */
+   loadImage(url) {
+      this.url = null;
+      const image = new Image();
+      const container = this.element.querySelector('.contentLB');
+      const loader = document.createElement('div');
+      loader.classList.add('lightbox__loader');
+      loader.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"style = "margin: auto; background: none; display: block; shape-rendering: auto;" width = "257px" height = "257px" viewBox = "0 0 100 100" preserveAspectRatio = "xMidYMid" ><path d = "M17 50A33 33 0 0 0 83 50A33 34.6 0 0 1 17 50" fill = "#911616" stroke = "none"><animateTransform attributeName = "transform" type = "rotate" dur = "1.5384615384615383s" repeatCount = "indefinite" keyTimes = "0;1" values = "0 50 50.8;360 50 50.8"></animateTransform></path></svg>';
+      container.innerHTML = '';
+      container.appendChild(loader);
+      image.onload = () => {
+         container.removeChild(loader);
+         container.append(image);
+      }
+      image.src = url;
+   }
+
+   /**
+    * 
+    * @param {KeyboardEvent} e 
+    */
+   onKeyUp(e) {
+      if (e.key == 'Escape') {
+         this.close(e);
+      } else if (e.key == 'ArrowLeft') {
+         this.prev(e);
+      } else if (e.key == 'ArrowRight') {
+         this.next(e);
+      }
+   }
+
+   /**
+    * Ferme la Lightbox
+    * @param {MouseEvent/KeyboardEvent} e 
+    */
+   close(e) {
+      e.preventDefault()
+      this.element.classList.add('fadeOut');
+      window.setTimeout(() => {
+         this.element.parentElement.removeChild(this.element)
+      }, 500);
+      document.removeEventListener('keyUp', this.onKeyUp);
+   }
+
+   /**
+    * 
+    * @param {MouseEvent/KeyboardEvent} e 
+    */
+   next(e) {
+      e.preventDefault();
+      let i = this.images.findIndex(image => image === this.url);
+      if(i === this.images.length - 1) {
+         i = -1;
+      }
+      this.loadImage(this.images[i + 1]);
+   }
+
+      /**
+    * 
+    * @param {MouseEvent/KeyboardEvent} e 
+    */
+   prev(e) {
+      e.preventDefault();
+      let i = this.images.findIndex(image => image === this.url);
+      if(i === 0) {
+         i = this.images.length;
+      }
+      this.loadImage(this.images[i - 1]);
+   }
+
+   //*********BUILDDOM**********//
+   /**
+    * 
+    * @param {string} url url de l'image
+    * @return {HTMLElement}
+    */
+
    buildDOM(url) {
-      //___________//create
-      //LIGHTBOX___
       const lightbox = document.createElement('div');
-      const closeLB = document.createElement('button');
-      const nextLB = document.createElement('button');
-      const prevLB = document.createElement('button');
-      const contentLB = document.createElement('div');
-      const imgLB = document.createElement('img');
-      //___________//settings
-      //LIGHTBOX___
       lightbox.classList.add('lightbox');
-      closeLB.classList.add('lightbox__closeLB');
-      closeLB.innerHTML = "Fermer";
-      nextLB.classList.add('lightbox__nextLB');
-      nextLB.innerHTML = "Suivant";
-      prevLB.classList.add('lightbox__prevLB');
-      prevLB.innerHTML = "Précedent";
-      contentLB.classList.add('lightbox__contentLB');
-      // imgLB.src = "";
-      //___________//indent
-      //LIGHTBOX___
-      main.append(lightbox);
-      lightbox.append(closeLB);
-      lightbox.append(nextLB);
-      lightbox.append(prevLB);
-      lightbox.append(contentLB);
-      contentLB.append(imgLB);
+      lightbox.innerHTML = '<button class="lightbox__closeLB">Fermer</button><button class="lightbox__nextLB">Suivant</button><button class="lightbox__prevLB">Précedent</button><div class="contentLB"></div>'
+      lightbox.querySelector('.lightbox__closeLB').addEventListener('click', this.close.bind(this));
+      lightbox.querySelector('.lightbox__nextLB').addEventListener('click', this.next.bind(this));
+      lightbox.querySelector('.lightbox__prevLB').addEventListener('click', this.prev.bind(this));
+      return lightbox;
    }
+
 }
 
-lightbox.init();
+lightbox.init()
 
 //******************* FORMULAIR *********************/
 //_____________//create
